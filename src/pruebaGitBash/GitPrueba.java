f4745d836b307646d0bc7084f9ba97576451492e
Jose Maria <Jose Maria@CHRISTIAN>
2017-01-30 16:57:27 +0100

    Primera versión

---
+++ src/pruebaGitBash/GitPrueba.java
@@ -0,0 +1,29 @@
+package pruebaGitBash;
+
+import java.util.Scanner;
+
+public class GitPrueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
+		double nota;
+
+		System.out.println("Introduce nota");
+		nota = teclado.nextDouble();
+
+		if (nota >= 0 && nota < 5) {
+			System.out.println("Suspenso");
+		} else if (nota >= 5 && nota < 7) {
+			System.out.println("Aprobado");
+		} else if (nota >= 7 && nota < 9) {
+			System.out.println("Notable");
+		} else if (nota >= 9 && nota <= 10) {
+			System.out.println("Sobresaliente");
+		} else {
+			System.out.println("nota incorrecta");
+		}
+
+	}
+
+}
