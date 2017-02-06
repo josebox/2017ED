97c7ab5fe8c62f82cf750115714f6445355ccdba
Jose Maria <Jose Maria@JoseMariaDeFederico>
2017-02-06 16:26:48 +0100

    Añadido gato Rama1

---
+++ src/pruebaGitBash/Gato.java
@@ -0,0 +1,15 @@
+package pruebaGitBash;
+
+public class Gato {
+
+	int numero_vidas;
+
+	public int getNumero_vidas() {
+		return numero_vidas;
+	}
+
+	public void setNumero_vidas(int numero_vidas) {
+		this.numero_vidas = numero_vidas;
+	}
+	
+}
